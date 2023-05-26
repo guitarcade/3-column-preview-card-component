2b5d4dda249b1c27efde302ef74fdadcc0260ecf
Arik Cohen <me@arikcohen.com>
2023-05-26 00:06:51 -0400

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["*.html"],
   theme: {
     colors: {
+      'bright-orange': 'hsl(31, 77%, 52%)',
+      'cyan-1': 'hsl(184, 100%, 22%)',
+      'cyan-2': 'hsl(179, 100%, 13%)',
+      'white': 'hsl(0, 0%, 100%)',
     },
     extend: {
       fontFamily: {
+        'big-shoulders-display'  : ['Big Shoulders Display', 'cursive'],
+        'lexend-deca'  : ['Lexend Deca', 'sans-serif'],
       },
     },
   },
